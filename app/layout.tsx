657ccf1d2893217cfe3a48b4ff28ea8fc4b2ba59
unknown <liuba@starmerx.com>
2026-09-17 06:19:17 +0000

    Add GoogleAnalytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +24,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <GoogleAnalytics gaId="G-RGDGHXDW4L" />
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
